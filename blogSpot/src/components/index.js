a37a6f3163ae03ac0796abc9dd81fad3a08241c0
mamathadevadiga04 <mamathadevadiga2001@gmail.com>
2024-12-26 23:27:35 +0530

    first commit

---
+++ blogSpot/src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./header/header";
+import Footer from "./footer/footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from './Signup';
+import Login from'./Login';
+import PostForm from "./POST-FORM/PostForm";
+import PostCard from './PostCard'
+import AuthLayout from './AuthLayout'
+import Button from './Button'
+import Input from './Input'
+import Select from './Select.jsx'
+export {
+    Header,
+    Footer,
+    Container,
+    LogoutBtn,
+    Logo,
+    RTE,
+    Select,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Button,
+    Input,
+
+}
