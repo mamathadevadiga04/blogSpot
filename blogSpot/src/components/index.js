3d7d6a23f9b00fe336ef0d0a6932e69bd232900d
mamathadevadiga04 <mamathadevadiga2001@gmail.com>
2024-12-27 00:18:25 +0530

    first commit

--- blogSpot/src/components/index.js
+++ blogSpot/src/components/index.js
@@ -1,5 +1,5 @@
 import Header from './Header/Header.jsx';
-import Footer from "./footer/footer";
+import Footer from "./Footer/Footer.jsx";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
